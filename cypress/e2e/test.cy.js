c32cbb2e346ea2e7f1c8fcfbca15553d726f6095
dzaja123 <nikolacardzajevic@gmail.com>
2022-10-04 14:27:41 +0200

    Added test scenarios

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,50 @@
-describe('Test 1', () => {
-    it('Test', () => {
+describe('Swag Labs', () => {
+
+    describe('1. Login Page', () => {
+
+        it('1.1. Login page loads properly and all elements are displayed properly.', () => {
+
+        })
+
+        it('1.2. Verify if a user will be able to login with a valid username and valid password.', () => {
+
+        })
+
+        it('1.3. Verify if a user cannot login with an invalid username and an invalid password.', () => {
+
+        })
+
+        it('1.4. Proper error message is displayed when user tries to login with wrong credentials.', () => {
+
+        })
 
     })
+
+    describe('2. Inventory Page', () => {
+
+        it('2.1. User is able to login successfuly.', () => {
+
+        })
+
+        it('2.2. User is able to add items to the cart.', () => {
+
+        })
+
+        it('2.3. User is able to check if items are added to the cart.', () => {
+
+        })
+
+        it('2.4. User is able to remove item from the cart.', () => {
+
+        })
+
+        it('2.5. User is able to continue the Checkout.', () => {
+
+        })
+
+
+
+    })
+
+
 })
