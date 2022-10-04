a9adaa4ebb70b3ee8b86feea6cd4f1e508ab9470
dzaja123 <nikolacardzajevic@gmail.com>
2022-10-04 14:42:18 +0200

    Update test Scenarios

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,48 +1,38 @@
 describe('Swag Labs', () => {
 
-    describe('1. Login Page', () => {
-
-        it('1.1. Login page loads properly and all elements are displayed properly.', () => {
-
-        })
-
-        it('1.2. Verify if a user will be able to login with a valid username and valid password.', () => {
-
-        })
-
-        it('1.3. Verify if a user cannot login with an invalid username and an invalid password.', () => {
-
-        })
-
-        it('1.4. Proper error message is displayed when user tries to login with wrong credentials.', () => {
-
-        })
+    it('1. Login page loads properly and all elements are displayed properly.', () => {
 
     })
 
-    describe('2. Inventory Page', () => {
+    it('2. Verify if a user will be able to login with a valid username and valid password.', () => {
 
-        it('2.1. User is able to login successfuly.', () => {
+    })
 
-        })
+    it('3. Verify if a user cannot login with an invalid username and an invalid password.', () => {
 
-        it('2.2. User is able to add items to the cart.', () => {
+    })
 
-        })
+    it('4. Proper error message is displayed when user tries to login with wrong credentials.', () => {
 
-        it('2.3. User is able to check if items are added to the cart.', () => {
+    })
 
-        })
+    it('5. User is able to login successfuly.', () => {
 
-        it('2.4. User is able to remove item from the cart.', () => {
+    })
 
-        })
+    it('6. User is able to add items to the cart.', () => {
 
-        it('2.5. User is able to continue the Checkout.', () => {
+    })
 
-        })
+    it('7. User is able to check if items are added to the cart.', () => {
 
+    })
 
+    it('8. User is able to remove item from the cart.', () => {
+
+    })
+
+    it('9. User is able to continue the Checkout.', () => {
 
     })
 
